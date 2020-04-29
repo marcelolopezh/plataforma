8622170a687cbc1abe28b0988993fee6cb5097af
marcelolopezh <marcelolopezhernandez@gmail.com>
2020-04-29 02:39:22 -0400

    CRUD COMPLETO

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <v-app>
     <div class="container">
+      <h1 class="text-center col-md-12">Tareas Administrativas</h1>
+      <hr>
       <Tareas/>
+
+      <hr>
     </div>
   </v-app>
 </template>
 
 <script>
 import Tareas from './components/Tareas'
+import HelloWorld from './components/HelloWorld'
 export default {
   name: 'App',
 
   components: {
-    Tareas
+    Tareas,
+    HelloWorld
   },
 
   data: () => ({
--- src/components/Tareas.vue
+++ src/components/Tareas.vue
@@ -2,13 +2,13 @@
     <div>
         <v-simple-table>
             <thead>
-            <tr>
+            <tr class="t">
                 <th style="display:hidden">#</th>
                 <th class="text-left">Encargado</th>
                 <th class="text-left">Tarea</th>
                 <th class="text-left">Estado</th>
                 <th class="text-left">Horas</th>
-                <th class="text-left">Acciones</th>
+                <th class="text-center">Acciones</th>
             </tr>
             </thead>
             <tbody>
@@ -18,18 +18,23 @@
                 <td>{{ item.descripcion }}</td>
                 <td>{{ item.estado }}</td>
                 <td>{{ item.horas }}</td>
-                <td>
-                <v-btn class="mx-2" fab dark small color="teal">
-                    <v-icon dark @click="deleteTarea(item)">mdi-delete</v-icon>
-                </v-btn>
-            </td>
+                <td class="text-center">
+                    <v-btn class="mx-2" fab dark small color="teal">
+                        <v-icon dark @click="deleteTarea(item)">mdi-delete</v-icon>
+                    </v-btn>
+                    <v-btn class="mx-2" fab dark small color="blue">
+                        <v-icon dark @click="[editTarea(item), dialogEdit = true]">mdi-pen</v-icon>
+                    </v-btn>
+                </td>
             </tr>
             </tbody>
+            <!-- BOTON PARA AGREGAR TAREAS -->
             <v-btn class="mx-2 text-center" fab dark small color="indigo" @click="dialog = true">
                 <v-icon dark>mdi-plus</v-icon>
             </v-btn>
         </v-simple-table>
-        <!-- MODAL PARA AGREGAR TAREAS  -->
+
+        <!-- MODAL PARA AGREGAR -->
         <v-dialog max-width="50%" v-model="dialog">
             <v-card>
                 <v-container>
@@ -57,6 +62,35 @@
                 </v-container>
             </v-card>
         </v-dialog>
+
+        <!-- MODAL PARA EDITAR TAREAS -->
+         <v-dialog max-width="50%" v-model="dialogEdit">
+            <v-card>
+                <v-container>
+                    <h1>Editar Tarea</h1>
+                    <v-form @submit.prevent="">
+                        <v-text-field type="text" label="Nombre Encargado" v-model="encargado"></v-text-field>
+                        <v-text-field type="text" label="Tarea" v-model="tarea"></v-text-field>
+                        <v-text-field type="text" label="Estado" v-model="estado"></v-text-field>
+                        <v-text-field type="number" label="Horas Estimadas" v-model="horas"></v-text-field>
+                        <v-btn
+                            color="success"
+                            class="mr-4"
+                            @click="insertUpdate(id)"
+                        > 
+                            Editar Tarea
+                        </v-btn>
+                        <v-btn
+                            color="error"
+                            class="mr-4"
+                            @click="stopModal()"
+                        > 
+                            Cerrar
+                        </v-btn>
+                    </v-form>
+                </v-container>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
@@ -68,10 +102,12 @@ export default {
 
             // VARIABLES PARA AGREGAR UN EVENTO //
             dialog : false,
+            dialogEdit : false,
             encargado : null,
             tarea : null,
             estado : null,
             horas : null,
+            id : null,
             /////////////////////////////////////
 
             headers : [
@@ -103,7 +139,7 @@ export default {
                    });
 
                     this.getTareas() // -> Cuando se agreguen los datos, se vuelve a llenar nuestro array
-                    this.clearVars() // -> Limpiamos las variables
+                    this.clearVars()
 
                }else{
                    console.log("Campos Vacios")
@@ -135,16 +171,40 @@ export default {
            }
            this.getTareas();
        },
+       async insertUpdate(id){
+           try{
+               await db.collection('tareas').doc(id).update({
+                   encargado : this.encargado,
+                   estado : this.estado,
+                   horas : this.horas,
+                   descripcion : this.tarea
+               })
+           }catch(error){
+               console.log(error)
+           }
+           this.stopModal();
+           this.clearVars();
+           this.getTareas();
+        },
+       editTarea(item){
+           this.encargado = item.encargado
+           this.estado = item.estado
+           this.horas = item.horas
+           this.tarea = item.descripcion  
+           this.id = item.id
+       },
        stopModal(){
            //FUNCION QUE CONDICIONA APERTURA DE MODAL
            this.dialog = false
+           this.dialogEdit = false
        },
        clearVars(){
            //LIMPIAMOS LAS VARIABLES PARA QUE EN EL MODAL NO APAREZCAN ESCRITAS
-           this.tareas = null
+           this.tarea = null
            this.encargado = null
            this.horas = null
-           this.descripcion = null
+           this.estado = null
+           this.id = null
        }
    }
 }
